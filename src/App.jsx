ec517a1d45d0682f90ad3adaabd0df5fd550d9c2
davidilejeune <davidilejeune@aol.com>
2023-07-24 19:35:33 -0400

    Fully functioning app

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,35 @@ function App() {
 
   const [featPupId, setFeatPupId] = useState(null)
 
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+    console.log(featuredPup)
+
+
   return (
     <>
      <div className="App">
      {
       puppies.map((puppy) => {
-        return <p>{puppy.name}</p>
+        return <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
     })
   }
+  
+  {featPupId  && (
+    <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>
+)
+}
+     
      </div>
         
     </>
-  );
+  )
 }
 
 export default App
+
