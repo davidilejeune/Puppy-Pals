cc60e602443f492c2ee96f6bb3b5451f21d14229
davidilejeune <davidilejeune@aol.com>
2023-07-24 14:32:08 -0400

    Added in functions to access and display puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
+
+  const [featPupId, setFeatPupId] = useState(null)
 
   return (
     <>
-     
+     <div className="App">
+     {
+      puppies.map((puppy) => {
+        return <p>{puppy.name}</p>
+    })
+  }
+     </div>
         
     </>
-  )
+  );
 }
 
 export default App
